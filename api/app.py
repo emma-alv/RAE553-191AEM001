e495df8eae5e1344d58ed04de6c236778f5da99f
Emmanuel Rodriguez <ing.erodrigueza@gmail.com>
2019-10-21 13:23:38 +0300

    Adding GET and POST request for specific items in the list

--- api/app.py
+++ api/app.py
@@ -1,25 +1,29 @@
 from flask import Flask
-from flask_restful import Resource, Api
-import json
-
-Catalog = {}
+from flask_restful import Resource, Api, abort
 
 app = Flask(__name__)
 api = Api(app)
 
+Items = {"items": []}
+
+def abort_if_item_doesnt_exist(item_name):
+    if item_name not in Items:
+        abort(404, message="Item {} doesn't exist".format(item_name))
+
 class Catalog(Resource):
     def get(self):
-        app_json = json.dumps(Catalog)
-        return app_json
+        return Items
 
 class Item(Resource):
     def get(self, item_name):
-        return Catalog[item_name]
+        abort_if_item_doesnt_exist(item_name)
+        return Items[item_name]
     def post(selg, item_name):
-        Catalog[item_name] = {'price': ''}
+        Items['items'].append(item_name)
+        Items[item_name] = {'price': ''}
+        return "Item {} succesfull added".format(item_name)
 
-api.add_resource(Catalog, '/catalog') #http://127.0.0.1:5000/catalog/
-api.add_resource(Item, '/item/<string:item_name>')
+api.add_resource(Catalog, '/items') # http://127.0.0.1:5000/Items/
+api.add_resource(Item, '/item/<string:item_name>') # http://127.0.0.1:5000/item/≤item_name>
 
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port=5000)
+app.run(port=5000)
