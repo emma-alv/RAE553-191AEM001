e5efd8ae5e0832d02f1b5885f65fd5231c7832a6
Emmanuel Rodriguez <ing.erodrigueza@gmail.com>
2019-10-24 16:58:55 +0300

    Adding reqparse to the POST request to get value for the "price" argument

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Resource, Api, abort
+from flask_restful import Resource, Api, abort, reqparse
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,20 +15,23 @@ def abort_if_item_doesnt_exist(item_name):
 
 class Items(Resource):
     def get(self):
-        return items["items"]
+        return items["items"], 201
 
 class Item(Resource):
     def get(self, item_name):
         abort_if_item_doesnt_exist(item_name)
-        return items[item_name]
+        return items[item_name], 201
     def post(selg, item_name):
         if item_name in items['items']:
             id = items[item_name]['id']
         else:
             id = len(items["items"]) + 1
             items['items'].append(item_name)
-        items[item_name] = {"id": id, "price": ""}
-        return "Item {} succesfull added".format(item_name)
+        parser = reqparse.RequestParser()
+        parser.add_argument("price")
+        args = parser.parse_args()
+        items[item_name] = {"id": id, "price": args["price"]}
+        return "Item {} succesfull updated".format(item_name), 201
 
 api.add_resource(Items, '/items') # http://127.0.0.1:5000/items/
 api.add_resource(Item, '/item/<string:item_name>') # http://127.0.0.1:5000/item/≤item_name>
